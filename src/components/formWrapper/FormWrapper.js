02e18298d9962ba310ac666d19925c4974663dd6
weronka341 <36929275+weronka341@users.noreply.github.com>
2019-05-16 12:28:17 +0200

    Update form

--- src/components/formWrapper/FormWrapper.js
+++ src/components/formWrapper/FormWrapper.js
@@ -1,12 +1,40 @@
+/* global google*/
 import React from "react";
 import './FormWrapper.css'
 import InputWrapper from "../inputWrapper/InputWrapper";
+import Script from 'react-load-script';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+import DayPicker from "react-day-picker";
+
 
 class FormWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pickUpAddress: '',
+      destinationAddress: '',
+    }
+  }
+
+  searchLocation() {
+    let input1 = document.getElementById('input1');
+    const autocomplete1 = new google.maps.places.Autocomplete(input1);
+    let input2 = document.getElementById('input2');
+    const autocomplete2 = new google.maps.places.Autocomplete(input2);
+
+  };
+
   render() {
     return (
       <div className='form-wrapper'>
-        <InputWrapper text='Please provide a street address, airport name or hotel name.'/>
+        <Script url="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMZQ0nW2HQ7bYl4ZLzNVqYuaHI7QSxbVY&libraries=places" onLoad={()=> this.searchLocation()}/>
+        <InputWrapper tip='Please provide a street address, airport name or hotel name.' text='Pick up:' inputId='input1'/>
+        <InputWrapper tip='Please provide a street address, airport name or hotel name.' text='Destination:' inputId='input2'/>
+        <div className='date-picker-wrapper'>
+         <input className='date-picker' type='date' placeholder={new Date()}/>
+        </div>
+
       </div>
     );
   }
--- src/components/inputWrapper/InputWrapper.js
+++ src/components/inputWrapper/InputWrapper.js
@@ -1,3 +1,4 @@
+/* global google*/
 import React from "react";
 import './InputWrapper.css';
 
@@ -24,9 +25,19 @@ class InputWrapper extends React.Component {
                width='23' onClick={() => {
             this.showHelper()
           }}/>
-          {this.state.showHelper && <span className='help-text'>{this.props.text}</span>}
+          {this.state.showHelper && <span className='help-text'>{this.props.tip}</span>}
         </label>
+
+        <span className='location-selector'>
+          <img alt='Arrow' src={process.env.PUBLIC_URL + '/arrow.svg'} width='47' height='27'/>
+        </span>
+        <label className='input-description'>{this.props.text}</label>
+        <div className='input-wrapper'>
+          <input id={this.props.inputId} className='input-style'/>
+        </div>
       </div>
+
+
     );
   }
 }
